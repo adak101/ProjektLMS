94a8bf4d03d1b506d168615b13ca88138147f500
Bartosz Surma <bartosz.surma@outlook.com>
2023-12-15 18:22:59 +0100

    Poprawienie struktury pakietu uzytkownikow

--- lms-api/src/main/java/pl/samba/lms/uzytkownicy/uzytkownicy/database/UzytkownikRepository.java
+++ lms-api/src/main/java/pl/samba/lms/uzytkownicy/uzytkownicy/database/UzytkownikRepository.java
@@ -1,4 +1,4 @@
-package pl.samba.lms.uzytkownicy.uzytkownicy.database.database;
+package pl.samba.lms.uzytkownicy.uzytkownicy.database;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
