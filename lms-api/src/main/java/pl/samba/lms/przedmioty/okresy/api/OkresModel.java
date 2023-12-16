bde3c588ce2474478fc1d1835edad28c04abfe16
Bartosz Surma <bartosz.surma@outlook.com>
2023-12-16 20:09:02 +0100

    Dodanie pola id do modeli

--- lms-api/src/main/java/pl/samba/lms/przedmioty/okresy/api/OkresModel.java
+++ lms-api/src/main/java/pl/samba/lms/przedmioty/okresy/api/OkresModel.java
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 
 @Getter
 public class OkresModel extends RepresentationModel<OkresModel> {
+    private final Integer id;
     private final String kod;
     private final LocalDateTime dataPoczatku;
     private final LocalDateTime dataKonca;
 
 
     public OkresModel(Okres o){
+        this.id = o.getIdOkresu();
         this.kod = o.getKod();
         this.dataPoczatku = o.getDataPoczatku();
         this.dataKonca = o.getDataKonca();
