ab4e7f97e65f277ad4c92cd1ae454fa75f9394df
Bartosz Surma <bartosz.surma@outlook.com>
2023-12-17 19:54:42 +0100

    Aktualizacja readme.md i wygenerowanie pierwszej paczki

--- lms-api/src/main/java/pl/samba/lms/LmsApplication.java
+++ lms-api/src/main/java/pl/samba/lms/LmsApplication.java
@@ -2,8 +2,10 @@ package pl.samba.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "pl.samba.lms")
 public class LmsApplication {
 
 	public static void main(String[] args) {
